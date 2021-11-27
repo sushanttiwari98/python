afa5ed8ef7102eb9fd97a22e0bbb59ab3efa054e
sushanttiwari98 <55651874+sushanttiwari98@users.noreply.github.com>
2021-11-27 10:43:09 +0530

    plotting of crowd computing

---
+++ pyplot_use.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 27 10:21:25 2021
+
+@author: asus
+"""
+
+import matplotlib.pyplot as plt
+plt.plot([1,2,3,4],[1,4,9,16])
+plt.ylabel('Square')
+plt.xlabel('Number')
+plt.plot([1,2,3,4],[1,4,9,16],'ro')
+plt.plot([1,2,3,4],[1,4,9,16],'r--')
+plt.plot([1,2,3,4],[1,4,9,16],'bs')
